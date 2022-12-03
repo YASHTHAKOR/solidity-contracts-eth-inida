174287e7dd8ddb0a4d67594bf60e591f549c3307
Kunjal Tandel <kunjal.tandel24@gmail.com>
2022-12-03 18:15:10 +0530

    CollectionFactory test case

--- test/Collection.ts
+++ test/Collection.ts
@@ -1,13 +1,12 @@
-import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import {Collection, Collection__factory} from "../typechain-types";
 
 describe("Mint token", function () {
-    let collection:any;
+    let collection: Collection;
     it("should deploy Contract", async function () {
         const [address1, address2] = await ethers.getSigners();
-        const Collection = await ethers.getContractFactory("Collection");
+        const Collection: Collection__factory = await ethers.getContractFactory("Collection");
         collection = await Collection.deploy("TEST", "test1", 5, 5, address1.address, address2.address);
         await collection.deployed();
     });
--- test/Factory.ts
+++ test/Factory.ts
@@ -1,38 +1,58 @@
-import { expect } from 'chai';
-import { ethers } from 'hardhat';
-import { Signer } from "ethers";
+import {expect} from 'chai';
+import {ethers} from 'hardhat';
+import {Contract, Signer} from 'ethers';
 
-import { CollectionFactory, CollectionFactory__factory } from '../typechain-types'
+import {abi as CollectionABI} from '../artifacts/contracts/Collection.sol/Collection.json'
+
+import {Collection, CollectionFactory, CollectionFactory__factory} from '../typechain-types'
 
 describe('Collection Factory', function () {
     let cf: CollectionFactory;
     let address1: Signer, address2: Signer, address3: Signer;
     let collections: string[] = [];
-    const setup = async () =>  {
+    const setup = async () => {
         const CF: CollectionFactory__factory = await ethers.getContractFactory('CollectionFactory');
         cf = await CF.deploy();
         await cf.deployed();
         [address1, address2, address3] = await ethers.getSigners()
     }
     beforeEach(setup)
-    it('should deploy Contract', async ()  => {
+    it('should deploy Contract', async () => {
         const CF: CollectionFactory__factory = await ethers.getContractFactory('CollectionFactory');
         cf = await CF.deploy();
         await cf.deployed();
     });
 
     it('can create collection', async () => {
-       const createCollectionTX = await cf.connect(address1).createCollection('Collection1', 'COL-1', 2, 2);
-       const createCollectionReceipt = await createCollectionTX.wait();
+        const createCollectionTX = await cf.connect(address1).createCollection('Collection1', 'COL-1', 2, 2);
+        const createCollectionReceipt = await createCollectionTX.wait();
 
-       console.log('collection receipt: ', JSON.stringify(createCollectionReceipt));
+        const collectionAddress = ((createCollectionReceipt.events || []).find((e) => e.event === 'CollectionCreated') || {args: {Collection: null}}).args?.Collection;
+
+        expect(typeof collectionAddress).to.be.eq('string', 'created collection is not present');
+
+        collections.push(collectionAddress);
     });
 
-    it('can clone collection', async () => {
-       const createCollectionTX = await cf.connect(address1).createCollection('Collection1', 'COL-1', 2, 2);
-       const createCollectionReceipt = await createCollectionTX.wait();
+    it('can not fork if minting is not completed', async () => {
+        const forkedCollection = await cf.connect(address1).forkCollection('Collection1', 'COL-1', collections[0])
+            .catch(e => e);
+    })
 
-       console.log('collection receipt: ', JSON.stringify(createCollectionReceipt));
+    it('can clone collection', async () => {
+        const forkCollectionTX = await cf.connect(address1).forkCollection('Collection1', 'COL-1', collections[0]);
+        const forkCollectionReceipt = await forkCollectionTX.wait();
+
+        const forkedCollectionAddress = ((forkCollectionReceipt.events || []).find((e) => e.event === 'CollectionCreated') || {args: {Collection: null}}).args?.Collection;
+
+        expect(typeof forkedCollectionAddress).to.be.eq('string', 'created collection is not present');
+
+        const forkedContract: Contract = new ethers.Contract(forkedCollectionAddress, CollectionABI)
+
+        const parent = await forkedContract.Parent();
+
+        expect(parent).to.be.eq(collections[0], 'created collection is not present');
+        collections.push(forkedCollectionAddress);
     });
 });
 
